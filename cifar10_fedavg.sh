f4c88e60c5918e7676c405ff6d2a74bafe9ca521
Fei Wang <silviafey.wang@utoronto.ca>
2022-03-18 16:06:01 -0400

    Upload  sbatch scripts for cc

---
+++ cifar10_fedavg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=08:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:2
+#SBATCH --mem=128G
+#SBATCH --account=def-baochun
+#SBATCH --output=print.out
+
+
+module load gcc/9.3.0 arrow cuda/11 python/3.9 scipy-stack
+source ~/.federated/bin/activate
+./run -c examples/fei/configs/CIFAR10/fedavg_resnet18.yml
---
+++ emnist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=08:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:2
+#SBATCH --mem=128G
+#SBATCH --account=def-baochun
+#SBATCH --output=print.out
+
+
+module load gcc/9.3.0 arrow cuda/11 python/3.9 scipy-stack
+source ~/.federated/bin/activate
+python examples/fei/fei.py -c examples/fei/configs/EMNIST/fei_lenet5.yml
