82fd168a7f3ee13df4efd03cf1f48fd6571c06f4
Fei Wang <917522022@qq.com>
2022-04-29 01:47:59 -0400

    Update configs

--- fei_vgg16_sim.sh
+++ fei_vgg16_sim.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=20:00:00
 #SBATCH --cpus-per-task=20
 #SBATCH --gres=gpu:1
-#SBATCH --mem=96G
-#SBATCH --output=print.out
+#SBATCH --mem=48G
+#SBATCH --output=fei_vgg16.out
 
 
 CUDA_LAUNCH_BLOCKING=1 python examples/fei/fei.py  -c examples/fei/configs/CIFAR10/fei_vgg16.yml
