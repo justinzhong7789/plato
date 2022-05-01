161d9bad7bd4300baee2bccb4d9b87e77c7e30c6
Fei Wang <silviafey.wang@utoronto.ca>
2022-05-01 00:05:58 -0400

    Update configs and sbatch scripts

--- cifar10_train_sim.sh
+++ cifar10_train_sim.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --gres=gpu:1
 #SBATCH --mem=96G
-#SBATCH --output=print.out
+#SBATCH --output=train.out
 
 
 CUDA_LAUNCH_BLOCKING=1 python examples/fei/fei.py  -c examples/fei/configs/CIFAR10/fei_resnet18_train.yml
---
+++ fashionmnist_lenet5_fedatt_sim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time=8:00:00
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:1
+#SBATCH --mem=48G
+#SBATCH --output=fedatt_lenet5.out
+
+
+CUDA_LAUNCH_BLOCKING=1 python examples/fedatt/fedatt.py  -c examples/fedatt/fedatt_FashionMNIST_lenet5.yml
