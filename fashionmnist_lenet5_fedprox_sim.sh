1052533b2827aa6c9540dce02608ead5befe294d
Fei Wang <917522022@qq.com>
2022-05-03 15:02:18 -0400

    Update configs and sbatch jobs

--- fashionmnist_lenet5_fedprox_sim.sh
+++ fashionmnist_lenet5_fedprox_sim.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --time=8:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:1
 #SBATCH --mem=48G
 #SBATCH --output=fedprox_lenet5.out
 
--- fashionmnist_lenet5_fei_sim.sh
+++ fashionmnist_lenet5_fei_sim.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --time=8:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:1
 #SBATCH --mem=48G
 #SBATCH --output=fei_lenet5.out
 
