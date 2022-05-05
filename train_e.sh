b1d93d43bd8b7733bd5c330fa6117e2f813e405c
Fei Wang <silviafey.wang@utoronto.ca>
2022-05-05 11:01:06 -0400

    Update sbatch script

---
+++ train_e.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --time=30:00:00
+#SBATCH --cpus-per-task=20
+#SBATCH --gres=gpu:1
+#SBATCH --mem=80G
+#SBATCH --output=train.out
+
+python examples/fei/fei.py  -c examples/fei/configs/e_train.yml
